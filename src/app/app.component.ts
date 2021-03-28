0ee9ab84b381319e6585f25e6257cba68148e245
Hugo Alberto <albert.hugo@hotmail.com>
2021-03-28 00:19:09 -0600

    Se creo el layout principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faSun as sunDark } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoApp';
+  sunDark = sunDark;
+  faMoon = faMoon;
 }
